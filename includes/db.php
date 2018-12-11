573b0229d36eec689fca64e778aa5d4bd1174244
anmihovil <anmihovil@gmail.com>
2018-12-11 16:40:48 +0100

    First commit of CMS project

---
+++ includes/db.php
@@ -0,0 +1,22 @@
+<?php
+
+$db['db_host'] = "localhost";
+$db['db_user'] = "anmihovil";
+$db['db_pass'] = "Jeremi3125";
+$db['db_name'] = "cms";
+
+foreach ($db as $key => $value) {
+  // code of a function making constants!
+  define(mb_strtoupper($key), $value);
+}
+
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+if($connection){
+  //echo "Database connection established!";
+}else{
+  die("Database connection failed");
+}
+
+
+?>

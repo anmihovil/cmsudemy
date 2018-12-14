74fa56977199d57d6d74129b398c2ab81dd33c2c
anmihovil <anmihovil@gmail.com>
2018-12-14 22:46:17 +0100

    Sidebar widget update

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -20,23 +20,45 @@
 
     <!-- Blog Categories Well -->
     <div class="well">
+
+      <?php
+
+          $query = "SELECT * FROM categories LIMIT 4";
+
+          $sellect_categories_sidebar = mysqli_query($connection, $query);
+
+          // while($row = mysqli_fetch_assoc($sellect_categories_sidebar)){
+          //   $cat_title = $row['cat_title'];
+          //   echo "<li><a href='#'>{$cat_title}</a></li>";
+          // }
+      ?>
+
         <h4>Blog Categories</h4>
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-lg-12">
                 <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
+
+                  <?php
+
+                    while($row = mysqli_fetch_assoc($sellect_categories_sidebar)){
+                      $cat_title = $row['cat_title'];
+                      echo "<li><a href='#'>{$cat_title}</a></li>";
+                    }
+
+                  ?>
+                    <!-- <li><a href="#">Category Name</a>
                     </li>
                     <li><a href="#">Category Name</a>
                     </li>
                     <li><a href="#">Category Name</a>
                     </li>
                     <li><a href="#">Category Name</a>
-                    </li>
+                    </li> -->
                 </ul>
             </div>
 
             <!-- /.col-lg-6 -->
-            <div class="col-lg-6">
+            <!-- <div class="col-lg-6">
                 <ul class="list-unstyled">
                     <li><a href="#">Category Name</a>
                     </li>
@@ -47,16 +69,13 @@
                     <li><a href="#">Category Name</a>
                     </li>
                 </ul>
-            </div>
+            </div> -->
+
             <!-- /.col-lg-6 -->
         </div>
         <!-- /.row -->
     </div>
 
     <!-- Side Widget Well -->
-    <div class="well">
-        <h4>Side Widget Well</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-    </div>
-
-</div>
+    <?php include "widget.php";?>
+  </div>

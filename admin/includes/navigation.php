2132ea84e5988ae948ea9607aceba1c0ffd0a869
anmihovil <anmihovil@gmail.com>
2018-12-15 22:11:39 +0100

    Adding appropriate Icons

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -55,14 +55,14 @@
               <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Users<i class="fa fa-fw fa-caret-down"></i></a>
               <ul id="demo" class="collapse">
                   <li>
-                      <a href="#"> Dropdown Item</a>
+                      <a href="#"><i class="fa fa-fw fa-user"></i> Dropdown Item</a>
                   </li>
                   <li>
-                      <a href="#"> Dropdown Item</a>
+                      <a href="#"><i class="fa fa-fw fa-user"></i> Dropdown Item</a>
                   </li>
               </ul>
             <li>
-                <a href="index-rtl.html"><i class="fa fa-fw fa-dashboard"></i> Profile</a>
+                <a href="index-rtl.html"><i class="fa fa-fw fa-user"></i> Profile</a>
             </li>
         </ul>
     </div>

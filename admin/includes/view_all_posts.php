b5bb75b65bda05d023dae8dd0f478d4b5b0474a1
anmihovil <anmihovil@gmail.com>
2019-02-21 22:38:06 +0100

    Lesson 183.

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,13 +1,36 @@
 
+<?php 
+
+if(isset($_POST['checkBoxArray'])){
+  foreach($_POST['checkBoxArray'] as $postValueId){
+    $bulk_options = $_POST['bulk_options'];
+
+    switch($bulk_options){
+      case 'published':
+      $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id = {$postValueId} ";
+      $update_to_published_status = mysqli_query($connection, $query);
+      confirmQuery($update_to_published_status);
+      break;
+      case 'draft':
+      $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id = {$postValueId} ";
+      $update_to_draft_status = mysqli_query($connection, $query);
+      confirmQuery($update_to_draft_status);
+      break;
+    }
+  }
+}
+
+?>
+
 <form action="" method='post'>
   <table class="table table-bordered table-hover">
 
   <div id="bulkOptionsContainer" class="col-xs-3">
-    <select class="form-control" name="" id="">
+    <select class="form-control" name="bulk_options" id="">
       <option value="">Select Options</option>
-      <option value="">Publish</option>
-      <option value="">Draft</option>
-      <option value="">Delete</option>
+      <option value="published">Publish</option>
+      <option value="draft">Draft</option>
+      <option value="delete">Delete</option>
     </select>
   </div>
 
@@ -19,6 +42,7 @@
 
     <thead>
       <tr>
+        <th><input id="selectAllBoxes" type="checkbox"></th>
         <th>Id</th>
         <th>Category</th>
         <th>Title</th>
@@ -55,6 +79,12 @@
         $post_status=$row['post_status'];
 
         echo "<tr>";
+        ?>
+
+        <td><input class='checkBoxes' type='checkbox' name='checkBoxArray[]' value='<?php echo $post_id; ?>'></td>;
+
+        <?php 
+        
         echo "<td>$post_id</td>";
 
         $query = "SELECT * FROM categories WHERE cat_id ={$post_category_id} ";

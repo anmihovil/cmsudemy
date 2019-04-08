983ecb96cab607fb7901a00919c4f6dc3b447ba4
anmihovil <anmihovil@gmail.com>
2019-04-08 10:52:03 +0200

    Lesson 227.

--- admin/includes/edit_user.php
+++ admin/includes/edit_user.php
@@ -2,57 +2,67 @@
 
 if(isset($_GET['edit_user'])){
   $the_user_id = $_GET['edit_user'];
-}
-
-  $query = "SELECT * FROM users WHERE user_id = $the_user_id ";
-  $select_user_query = mysqli_query($connection, $query);
-
-  while($row = mysqli_fetch_assoc($select_user_query)){
-    $user_id        = $row['user_id'];
-    $user_firstname = $row['user_firstname'];
-    $user_lastname  = $row['user_lastname'];
-    $username       = $row['username'];
-    $user_role      = $row['user_role'];
-    $user_email     = $row['user_email'];
-    $user_password  = $row['user_password'];
-
-    if(isset($_POST['edit_user'])){
-
-    $user_firstname = $_POST['user_firstname'];
-    $user_lastname  = $_POST['user_lastname'];
-    $username       = $_POST['username'];
-    $user_role      = $_POST['user_role'];
-    $user_email     = $_POST['user_email'];
-    $user_password  = $_POST['user_password'];
-
-    // Code kept for reference - replaced by 
-    // $query = "SELECT randSalt FROM users";
-    // $select_randsalt_query = mysqli_query($connection, $query);
-    //   if(!$select_randsalt_query){
-    //     die("Query Failed!" . mysqli_error($connection));
-    //   }
-    // $row = mysqli_fetch_array($select_randsalt_query);
-    // $salt = $row['randSalt'];
-    // $hashed_password = crypt($user_password, $salt);
-
-    $hashed_password = password_hash( $user_password, PASSWORD_BCRYPT, array('cost' => 10 ));
-
-    $query = "UPDATE users SET ";
-    $query.= "user_firstname   = '{$user_firstname}', ";
-    $query.= "user_lastname    = '{$user_lastname}', ";
-    $query.= "username         = '{$username}', ";
-    $query.= "user_role        = '{$user_role}', ";
-    $query.= "user_email       = '{$user_email}', ";
-    $query.= "user_password    = '{$hashed_password}' ";
-    $query.= "WHERE user_id    =  {$the_user_id}";
 
 
-    $update_user = mysqli_query($connection, $query);
-  
-    confirmQuery($update_user);
-  } 
-}
-      
+    $query = "SELECT * FROM users WHERE user_id = $the_user_id ";
+    $select_user_query = mysqli_query($connection, $query);
+
+    while($row = mysqli_fetch_assoc($select_user_query)){
+      $user_id        = $row['user_id'];
+      $user_firstname = $row['user_firstname'];
+      $user_lastname  = $row['user_lastname'];
+      $username       = $row['username'];
+      $user_role      = $row['user_role'];
+      $user_email     = $row['user_email'];
+      $user_password  = $row['user_password'];
+
+      if(isset($_POST['edit_user'])){
+
+      $user_firstname = $_POST['user_firstname'];
+      $user_lastname  = $_POST['user_lastname'];
+      $username       = $_POST['username'];
+      $user_role      = $_POST['user_role'];
+      $user_email     = $_POST['user_email'];
+      $user_password  = $_POST['user_password'];
+
+      // Code kept for reference - replaced by 'password_hash()' function
+      // $query = "SELECT randSalt FROM users";
+      // $select_randsalt_query = mysqli_query($connection, $query);
+      //   if(!$select_randsalt_query){
+      //     die("Query Failed!" . mysqli_error($connection));
+      //   }
+      // $row = mysqli_fetch_array($select_randsalt_query);
+      // $salt = $row['randSalt'];
+      // $hashed_password = crypt($user_password, $salt);
+
+      if(!empty($user_password)){
+        $query_password = "SELECT user_password FROM users WHERE user_id = $the_user_id";
+        $get_user_query = mysqli_query($connection, $query);
+        confirmQuery($get_user_query);
+        $db_user_password = $row['user_password'];
+      }
+
+      if($db_user_password !== $user_password){
+        $hashed_password = password_hash( $user_password, PASSWORD_BCRYPT, array('cost' => 10 ));
+      }
+
+      $query = "UPDATE users SET ";
+      $query.= "user_firstname   = '{$user_firstname}', ";
+      $query.= "user_lastname    = '{$user_lastname}', ";
+      $query.= "username         = '{$username}', ";
+      $query.= "user_role        = '{$user_role}', ";
+      $query.= "user_email       = '{$user_email}', ";
+      $query.= "user_password    = '{$hashed_password}' ";
+      $query.= "WHERE user_id    =  {$the_user_id}";
+
+
+      $update_user = mysqli_query($connection, $query);
+    
+      confirmQuery($update_user);
+    } 
+  }
+} 
+
 ?>
 
 <form action="" method="post" enctype="multipart/form-data">
